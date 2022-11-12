f37fff5932b419e1d59ec89e23c6b7cca07430ed
Chaka Mofokeng <mofokeng.chk@gmail.com>
2022-11-12 05:15:28 -0500

    Fix flake8 errors

--- service/routes.py
+++ service/routes.py
@@ -56,6 +56,7 @@ def create_accounts():
         jsonify(message), status.HTTP_201_CREATED, {"Location": location_url}
     )
 
+
 ######################################################################
 # LIST ALL ACCOUNTS
 ######################################################################
@@ -69,7 +70,7 @@ def list_accounts():
 
     accounts = Account.all()
     account_list = [account.serialize() for account in accounts]
-    
+
     return make_response(
         jsonify(account_list), status.HTTP_200_OK
     )
@@ -139,12 +140,9 @@ def delete_accounts(account_id):
     return "", status.HTTP_204_NO_CONTENT
 
 
-
 ######################################################################
 #  U T I L I T Y   F U N C T I O N S
 ######################################################################
-
-
 def check_content_type(media_type):
     """Checks that the media type is correct"""
     content_type = request.headers.get("Content-Type")
