c0fbb9a7a5eda50b942b9ef5d7bf42e9668750b2
Chaka Mofokeng <mofokeng.chk@gmail.com>
2022-11-12 00:40:25 -0500

    test read an accout

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -125,26 +125,33 @@ class TestAccountService(TestCase):
 
     def test_read_an_account(self):
         """It should Read an Account"""
-        account = AccountFactory()
-        response = self.client.post(
-            BASE_URL,
-            json=account.serialize(),
-            content_type="application/json"
-        )
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        # Create new account
+        account = self._create_accounts(1)[0]
 
-        account_id = account.serialize()["id"]
-        resp = self.client.get(f"/{BASE_URL}/{account_id}")
-        self.assertEqual(resp.status_code, status.HTTP_200_OK)
-        print(resp)
-        new_account = resp.json()
+        # Get the newly created account by id
+        response = self.client.get(f"{BASE_URL}/{account.id}")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        new_account = response.get_json()
 
         # Check the data is correct
-        new_account = response.get_json()
+        self.assertEqual(new_account["id"], account.id)
         self.assertEqual(new_account["name"], account.name)
         self.assertEqual(new_account["email"], account.email)
         self.assertEqual(new_account["address"], account.address)
         self.assertEqual(new_account["phone_number"], account.phone_number)
         self.assertEqual(new_account["date_joined"], str(account.date_joined))
+
+    def test_read_non_existing_account(self):
+        """It should Read an Account the does not exist"""
+        account_id = 10000000
+
+        response = self.client.get(f"{BASE_URL}/{account_id}")
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+        message = response.get_json()["message"]
+
+        self.assertIn("could not be found", message)
+
+
 
 
