8e13298abfe75e85dfa4e33641956cbd366a941b
Chaka Mofokeng <mofokeng.chk@gmail.com>
2022-11-12 02:54:15 -0500

    test delete account

--- service/routes.py
+++ service/routes.py
@@ -136,7 +136,7 @@ def delete_accounts(account_id):
         return abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
 
     account.delete()
-    return jsonify(dict()), status.HTTP_204_NO_CONTENT
+    return "", status.HTTP_204_NO_CONTENT
 
 
 
